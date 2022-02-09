4917677e9b9e8166b76e965fd3b3ffa0ef8b1623
Pavlik Kiselev <paulcodiny@gmail.com>
2022-02-09 22:31:15 +0100

    Revert "update label switcher"
    This reverts commit e82e01ff89703bd2bbaa212993e3d4d57f8f390b.

--- src/components/basic/LabeledSwitcher/index.tsx
+++ src/components/basic/LabeledSwitcher/index.tsx
@@ -58,9 +58,6 @@ export default function LabeledSwitcher({
               background: ${currentTheme.white.hex};
             }
           }
-          &__pointer {
-            padding: 2px 1px !important;
-          }
 
           &__inner {
             background: #7159ff;
